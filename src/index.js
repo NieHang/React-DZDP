0fe7cf4aab8350e928159907e262671208a9bd6c
NieHang <1076883711@qq.com>
2019-02-21 19:37:02 +0800

    store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'redux';
+import store from './redux/store';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
