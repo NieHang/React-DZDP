bb83967f3664c7f270228f4cb9231be0a8f929b3
NieHang <1076883711@qq.com>
2019-02-22 10:31:00 +0800

    创建 加载猜你喜欢action

--- src/redux/modules/home.js
+++ src/redux/modules/home.js
@@ -1,5 +1,56 @@
-const reducer = (state ={}, action) => {
-  return state;
-}
+import { get } from '../../utils/request';
+import url from '../../utils/url';
+
+export const types = {
+  // 获取猜你喜欢请求
+  FETCH_LIKES_REQUEST: 'HOME/FETCH_LIKES_REQUEST',
+  // 获取猜你喜欢成功
+  FETCH_LIKES_SUCCESS: 'HOME/FETCH_LIKES_SUCCESS',
+  // 获取猜你喜欢失败
+  FETCH_LIKES_FAILURE: 'HOME/FETCH_LIKES_FAILURE'
+};
+
+export const actions = {
+  loadLikes: () => {
+    return (dispatch, getState) => {
+      dispatch(fetchLikesRequest());
+      return get(url.getProductList(0, 10)).then(
+        data => {
+          dispatch(fetchLikesSuccess(data));
+        },
+        error => {
+          dispatch(fetchLikesFailure(error));
+        }
+      );
+    };
+  }
+};
+
+const fetchLikesRequest = () => ({
+  type: types.FETCH_LIKES_REQUEST
+});
+
+const fetchLikesSuccess = data => ({
+  type: types.FETCH_LIKES_SUCCESS,
+  data
+});
+
+const fetchLikesFailure = error => ({
+  type: types.FETCH_LIKES_FAILURE,
+  error
+});
+
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case types.FETCH_LIKES_REQUEST:
+      break;
+    case types.FETCH_LIKES_SUCCESS:
+      break;
+    case types.FETCH_LIKES_FAILURE:
+      break;
+    default:
+      return state;
+  }
+};
 
 export default reducer;
