ff02b02f2dd8e31c91f15a1951117f72c8c36490
NieHang <1076883711@qq.com>
2019-03-06 09:46:15 +0800

    购买须知组件

--- src/containers/ProductDetail/index.js
+++ src/containers/ProductDetail/index.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import ProductOverview from './components/ProductOverview';
 import ShopInfo from './components/ShopInfo';
 import Detail from './components/Detail';
+import Remark from './components/Remark';
 
 export default class ProductDetail extends Component {
-  render() {
-    return (
-      <div>
-        <ProductOverview />
-        <ShopInfo />
-        <Detail />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<ProductOverview />
+				<ShopInfo />
+				<Detail />
+				<Remark />
+			</div>
+		);
+	}
 }
