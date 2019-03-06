2042f5ef166929f78fb5281a174039243613a70c
NieHang <1076883711@qq.com>
2019-03-06 10:32:16 +0800

    由createReducer创建reducer

--- src/redux/modules/entities/products.js
+++ src/redux/modules/entities/products.js
@@ -1,14 +1,11 @@
+import createReducer from '../../../utils/createReducer';
+
 export const schema = {
   // 表名
   name: 'products',
   id: 'id'
 }
 
-const reducer = (state = {}, action) => {
-  if (action.response && action.response.products) {
-    return { ...state, ...action.response.products }
-  }
-  return state;
-}
+const reducer = createReducer(schema.name);
 
 export default reducer;
