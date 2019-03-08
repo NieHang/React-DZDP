f458876ceb2b150658c91885f97beb01e7bf0fc7
NieHang <1076883711@qq.com>
2019-03-08 14:31:40 +0800

    SearchHeader和KeywordBox组件

--- src/containers/SearchResult/index.js
+++ src/containers/SearchResult/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import SearchHeader from './components/SearchHeader';
+import KeywordBox from './components/KeywordBox';
+import Banner from '../../components/Banner';
 import ShopList from './components/ShopList';
 
 export default class SearchResult extends Component {
 	render() {
 		return (
 			<div>
+				<SearchHeader onBack={this.handleBack} onSearch={this.handleSearch} />
+				<KeywordBox text="text" />
+				<Banner dark/>
 				<ShopList />
 			</div>
 		);
 	}
+
+	handleBack = () => {
+		this.props.history.push('/');
+	};
+
+	handleSearch = () => {
+		this.props.history.push('/search');
+	};
 }
