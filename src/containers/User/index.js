297a5f1be433fae6da90ac209b80dfbd00f59dcc
NieHang <1076883711@qq.com>
2019-03-11 15:52:27 +0800

    个人中心页UserHeader组件开发

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import UserMain from './components/UserMain';
+import UserHeader from './components/UserHeader';
 
 export default class User extends Component {
 	render() {
 		return (
 			<div>
+				<UserHeader onBack={this.handleBack} onLogout={this.handleLogout} />
 				<UserMain />
 			</div>
 		);
 	}
+
+	handleBack = () => {
+		// todo
+	};
+
+	handleLogout = () => {
+		// todo
+	};
 }
