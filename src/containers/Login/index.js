bb80105ed2555d6df6238d60d4a695a87669c64c
NieHang <1076883711@qq.com>
2019-03-11 14:07:03 +0800

    登录页连接redux

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Redirect } from 'react-router-dom';
+import {
+	getUsername,
+	getPassword,
+	isLogin,
+	actions as loginActions
+} from '../../redux/modules/login';
 
 import LoginHeader from './components/LoginHeader';
 import LoginForm from './components/LoginForm';
 
-export default class Login extends Component {
+class Login extends Component {
 	render() {
+		const { username, password, login } = this.props;
+		if (login) {
+			return <Redirect to="/user" />;
+		}
 		return (
 			<div>
 				<LoginHeader />
-				<LoginForm />
+				<LoginForm
+					username={username}
+					password={password}
+					onChange={this.handleChange}
+					onSubmit={this.handleSubmit}
+				/>
 			</div>
 		);
 	}
+
+	handleChange = e => {
+		if (e.target.name === 'username') {
+			this.props.loginActions.setUsername(e.target.value);
+		} else if (e.target.name === 'password') {
+			this.props.loginActions.setPassword(e.target.value);
+		}
+	};
+
+	// 登录
+  handleSubmit = () => {
+    this.props.loginActions.login();
+  };
 }
+
+const mapStateToProps = (state, props) => ({
+	usename: getUsername(state),
+	password: getPassword(state),
+	login: isLogin(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+	loginActions: bindActionCreators(loginActions, dispatch)
+});
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Login);
