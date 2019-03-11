f5dd753ca6fa0ed08caa7663548ae386eedd1b40
NieHang <1076883711@qq.com>
2019-03-11 15:47:59 +0800

    个人中心页OrderItem组件开发

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import UserMain from './components/UserMain';
 
 export default class User extends Component {
-  render() {
-    return (
-      <div>
-        个人中心
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<UserMain />
+			</div>
+		);
+	}
 }
