093be51d1669f5903778724a00c0e097df75f65d
NieHang <1076883711@qq.com>
2019-03-13 14:51:11 +0800

    订单删除功能（reducer编写）

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -6,6 +6,7 @@ import {
 	AVAILABLE_TYPE,
 	REFUND_TYPE,
 	getOrderById,
+	types as orderTypes,
 	actions as orderActions
 } from './entities/orders';
 import { combineReducers } from 'redux';
@@ -39,17 +40,20 @@ export const types = {
 	DELETE_ORDER_REQUEST: 'USER/DELETE_ORDER_REQUEST',
 	DELETE_ORDER_SUCCESS: 'USER/DELETE_ORDER_SUCCESS',
 	DELETE_ORDER_FAILURE: 'USER/DELETE_ORDER_FAILURE',
+	// 删除确认对话框
+	SHOW_DELETE_DIALOG: 'USER/SHOW_DELETE_DIALOG',
+	HIDE_DELETE_DIALOG: 'USER/HIDE_DELETE_DIALOG'
 };
 
 export const actions = {
 	// 获取订单列表
 	loadOrders: () => {
 		return (dispatch, getState) => {
-      const { ids } = getState().user.orders;
+			const { ids } = getState().user.orders;
 			if (ids.length > 0) {
 				return null;
 			}
-      const endpoint = url.getOrders();
+			const endpoint = url.getOrders();
 			return dispatch(fetchOrders(endpoint));
 		};
 	},
@@ -60,7 +64,7 @@ export const actions = {
 	}),
 	// 删除订单
 	removeOrder: () => {
-		return ( dispatch, getState ) => {
+		return (dispatch, getState) => {
 			const { id } = getState().user.currentOrder;
 			if (id) {
 				dispatch(deleteOrderRequest());
@@ -70,20 +74,29 @@ export const actions = {
 						dispatch(orderActions.deleteOrder(id));
 						resolve();
 					}, 500);
-				})
+				});
 			}
-		}
-	}
+		};
+	},
+	// 显示删除对话框
+	showDeleteDialog: orderId => ({
+		type: types.SHOW_DELETE_DIALOG,
+		orderId
+	}),
+	// 隐藏删除对话框
+	hideDeleteDialog: () => ({
+		type: types.HIDE_DELETE_DIALOG
+	})
 };
 
 const deleteOrderRequest = () => ({
 	type: types.DELETE_ORDER_REQUEST
-})
+});
 
 const deleteOrderSuccess = orderId => ({
 	type: types.DELETE_ORDER_SUCCESS,
 	orderId
-})
+});
 
 const fetchOrders = endpoint => ({
 	[FETCH_DATA]: {
@@ -102,7 +115,7 @@ const orders = (state = initialState.orders, action) => {
 	switch (action.type) {
 		case types.FETCH_ORDERS_REQUEST:
 			return { ...state, isFetching: true };
-    case types.FETCH_ORDERS_SUCCESS:
+		case types.FETCH_ORDERS_SUCCESS:
 			const toPayIds = action.response.ids.filter(
 				id => action.response.orders[id].type === TO_PAY_TYPE
 			);
@@ -111,7 +124,7 @@ const orders = (state = initialState.orders, action) => {
 			);
 			const refundIds = action.response.ids.filter(
 				id => action.response.orders[id].type === REFUND_TYPE
-      );
+			);
 			return {
 				...state,
 				isFetching: false,
@@ -120,9 +133,22 @@ const orders = (state = initialState.orders, action) => {
 				availableIds: state.availableIds.concat(availableIds),
 				refundIds: state.refundIds.concat(refundIds)
 			};
+		case orderTypes.DELETE_ORDER:
+		case types.DELETE_ORDER_SUCCESS:
+			return {
+				...state,
+				ids: removeOrderId(state, 'ids', action.orderId),
+				toPayIds: removeOrderId(state, 'toPayIds', action.orderId),
+				availableIds: removeOrderId(state, 'availableIds', action.orderId),
+				refundIds: removeOrderId(state, 'refundIds', action.orderId)
+			};
 		default:
 			return state;
 	}
+};
+
+const removeOrderId = (state, key, orderId) => {
+	return state[key].filter(id => id !== orderId);
 };
 
 const currentTab = (state = initialState.currentTab, action) => {
@@ -134,9 +160,27 @@ const currentTab = (state = initialState.currentTab, action) => {
 	}
 };
 
+const currentOrder = (state = initialState.currentOrder, action) => {
+	switch (action.type) {
+		case types.SHOW_DELETE_DIALOG:
+			return {
+				...state,
+				id: action.orderId,
+				isDeleting: true
+			};
+		case types.HIDE_DELETE_DIALOG:
+		case types.DELETE_ORDER_SUCCESS:
+		case types.FETCH_ORDERS_FAILURE:
+			return initialState.currentOrder;
+		default:
+			return state;
+	}
+};
+
 const reducer = combineReducers({
 	currentTab,
-	orders
+	orders,
+	currentOrder
 });
 
 export default reducer;
@@ -147,8 +191,14 @@ export const getCurrentTab = state => state.user.currentTab;
 export const getOrders = state => {
 	const key = ['ids', 'toPayIds', 'availableIds', 'refundIds'][
 		state.user.currentTab
-  ];
+	];
 	return state.user.orders[key].map(id => {
 		return getOrderById(state, id);
 	});
+};
+
+export const getDeletingOrderId = state => {
+	return state.user.currentOrder && state.user.currentOrder.isDeleting
+		? state.user.currentOrder.id
+		: null;
 };
