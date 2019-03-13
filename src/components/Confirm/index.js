e81203b701e3339cf9a07924a6c64ddd0ec35875
NieHang <1076883711@qq.com>
2019-03-13 09:32:57 +0800

    Confirm组件开发

---
+++ src/components/Confirm/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Confirm extends Component {
+	render() {
+		const {
+			content,
+			cancelText,
+			confirmText,
+			onCancel,
+			onConfirm
+		} = this.props;
+		return (
+			<div className="confirm">
+				<div className="confirm__alert">
+					<div className="confirm__content">{content}</div>
+					<div className="confirm__btns">
+						<a className="confirm__btn" onClick={onCancel}>
+							{cancelText}
+						</a>
+						<a className="confirm__btn" onClick={onConfirm}>
+							{confirmText}
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
