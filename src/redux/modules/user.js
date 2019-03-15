708a5fda392a075ae006f695f13c70e06847686a
NieHang <1076883711@qq.com>
2019-03-15 09:44:33 +0800

    集成Reselect，优化性能

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,4 +1,5 @@
 import url from '../../utils/url';
+import { createSelector } from 'reselect'
 import { FETCH_DATA } from '../middleware/api';
 import {
 	schema,
@@ -6,6 +7,7 @@ import {
 	AVAILABLE_TYPE,
 	REFUND_TYPE,
 	getOrderById,
+	getAllOrders,
 	types as orderTypes,
 	actions as orderActions
 } from './entities/orders';
@@ -287,14 +289,18 @@ export default reducer;
 // selectors
 export const getCurrentTab = state => state.user.currentTab;
 
-export const getOrders = state => {
-	const key = ['ids', 'toPayIds', 'availableIds', 'refundIds'][
-		state.user.currentTab
-	];
-	return state.user.orders[key].map(id => {
-		return getOrderById(state, id);
-	});
-};
+const getUserOrders = state => state.user.orders;
+
+export const getOrders = createSelector(
+	[getCurrentTab, getUserOrders, getAllOrders],
+	(tabIndex, userOrders, orders) => {
+		const key = ['ids', 'toPayIds', 'availableIds', 'refundIds'][tabIndex];
+		const orderIds = userOrders[key];
+		return orderIds.map(id => {
+			return orders[id]
+		})
+	}
+)
 
 export const getDeletingOrderId = state => {
 	return state.user.currentOrder && state.user.currentOrder.isDeleting
